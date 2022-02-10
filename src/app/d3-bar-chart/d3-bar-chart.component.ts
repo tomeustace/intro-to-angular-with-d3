79962c9fc6fb04922fdad17d332af1fe522a785e
Tom Eustace <tom@joulica.io>
2022-02-10 22:49:59 +0000

    Add bar charts

--- src/app/d3-bar-chart/d3-bar-chart.component.ts
+++ src/app/d3-bar-chart/d3-bar-chart.component.ts
@@ -2,6 +2,9 @@ import { Component, OnInit } from '@angular/core';
 import { select, selectAll } from 'd3-selection';
 import { axisLeft, axisBottom } from 'd3-axis';
 import { scaleLinear, scaleBand } from 'd3-scale';
+import transition from 'd3-transition';
+import * as d3 from 'd3';
+import { HighlightService } from '../highlight.service';
 @Component({
   selector: 'app-d3-bar-chart',
   templateUrl: './d3-bar-chart.component.html',
@@ -9,52 +12,106 @@ import { scaleLinear, scaleBand } from 'd3-scale';
 })
 export class D3BarChartComponent implements OnInit {
 
-  h = 200;
-  width = 20;
-  data = [{name: "bob", age: 20}, {name: "jane", age: 30}, {name: "john", age: 40}];
-  constructor() { }
+  height = 200;
+  width = 300;
+  data = [{name: "paul", age: 35},{name: "sue", age: 80}, {name: "fred", age: 5}, {name: "alice", age: 50}, {name: "bob", age: 90}, {name: "jane", age: 30}, {name: "john", age: 40}];
+
+  x = scaleBand()
+    .domain(this.data.map(d => d.name))
+    .range([0, this.width])
+    .padding(0.2);
+
+  y = scaleLinear()
+    .domain([0, 100])
+    .range([this.height, 0]);
+
+  constructor(private highlightService: HighlightService) { }
 
   ngOnInit(): void {
-    const x = scaleBand()
-      .domain(this.data.map(d => d.name))
-      .range([0, 100])
-      .padding(0.1);
+    select("#bar-chart-1")
+      .append("g")
+      .attr("transform", "translate(0," + this.height + ")")
+      .call(axisBottom(this.x));
 
-    const y = scaleLinear()
-      .domain([this.h, 0])
-      .range([0, 500]);
-
-    select('#bar-chart')
+    select('#bar-chart-1')
       .selectAll('rect')
       .data(this.data)
       .enter()
       .append('rect')
-          .attr("class", "bar")
-          .attr("x", (d) => {
-            console.log("x", x(d.name));
-            return x(d.name);
-          })
-          .attr("width", 20)
-          // .attr("width", x.bandwidth())
-          .attr("y", (d) => { return y(d.age) - this.h; })
-          .attr("height", (d) => {
-            const h = this.h + +y(d.age);
-            console.log(h);
-            console.log(typeof h);
-            return +h;
-          })
+          .attr("x", (d) =>this.x(d.name))
+          .attr("width", this.x.bandwidth())
+          .attr("y", (d) => { return this.height - this.y(d.age); })
+          .attr("height", (d) => this.y(d.age))
           .attr("fill", "steelblue");
-      // .attr('x', (d) => y(d))
-    // const selection = select("#bar-chart")
-    //   selectAll("rect")
-    //       .data(this.data);
-    //   console.log(selection);
 
-    //     selection.enter().append("rect")
-    //       .attr("class", "bar")
-    //       .attr("x", function(d) { return x(d); })
-    //       .attr("width", x.bandwidth())
-    //       .attr("y", function(d) { return y(d); })
-    //       .attr("height", function(d) { return 100 - y(d); });
+          this.drawBar2();
+          this.drawBar3();
+  }
+
+  ngAfterViewInit(): void {
+    this.highlightService.highlightAll();
+  }
+
+  drawBar2() {
+    select("#bar-chart-2").selectAll("rect").remove();
+    select("#bar-chart-2")
+      .append("g")
+      .attr("transform", "translate(0," + this.height + ")")
+      .call(axisBottom(this.x));
+
+    select('#bar-chart-2')
+      .selectAll('rect')
+      .data(this.data)
+      .enter()
+      .append('rect')
+          .attr("x", (d) => this.x(d.name))
+          .attr("width", this.x.bandwidth())
+          .attr("y", (d) => { return this.height - this.y(d.age); })
+          .attr("height", (d) => this.y(d.age))
+          .attr("fill", "steelblue")
+          .on("mouseover", function(d) {
+            select(this).transition().duration(1500).attr("fill", "red");
+          })
+          .on("mouseout", function(d) {
+            select(this).attr("fill", "green");
+          })
+          .on("click", function(d) {
+            d3.select(this).transition().duration(1000).attr("height", 0).attr("y", 200);
+          });
+
+  }
+  drawBar3() {
+    const margin = {top: 20, right: 20, bottom: 30, left: 40};
+    select("#bar-chart-3").selectAll("rect").remove();
+    select("#bar-chart-3")
+      .append("g")
+      .attr("transform", "translate(" + margin.left + "," + this.height + ")")
+      .call(axisBottom(this.x));
+
+    select("#bar-chart-3")
+      .append("g")
+      .attr("transform", "translate(" + margin.left + ",0)")
+      .call(axisLeft(this.y));
+
+    select('#bar-chart-3')
+      .selectAll('rect')
+      .data(this.data)
+      .enter()
+      .append('rect')
+          .attr("x", (d) => this.x(d.name) + margin.left)
+          .attr("width", this.x.bandwidth())
+          .attr("y", (d) => { return this.height - this.y(d.age); })
+          .attr("height", (d) => this.y(d.age))
+          .attr("fill", "steelblue")
+          .on("mouseover", function(d) {
+            select(this).transition().duration(1500).attr("fill", "red");
+          })
+          .on("mouseout", function(d) {
+            select(this).attr("fill", "green");
+          })
+          .on("click", function(d) {
+            d3.select(this).transition().duration(1000).attr("height", 0).attr("y", 200);
+          });
+
   }
 }
